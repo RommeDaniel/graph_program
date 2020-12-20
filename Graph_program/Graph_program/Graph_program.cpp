7797a99fb099801420835ff000b036832e23ad5c
dmorra18 <dmorra18@student.aau.dk>
2020-12-20 20:42:43 +0100

    Tilføjet Design noter og oprettet visual studio projekt
    VERSION 0.0.1

---
+++ Graph_program/Graph_program/Graph_program.cpp
@@ -0,0 +1,12 @@
+﻿// Graph_program.cpp : Defines the entry point for the application.
+//
+
+#include "Graph_program.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
